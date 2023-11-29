fd197110b22f631b78712af87d9e97384ff8f01c
Ortemka <numb432hz@gmail.com>
2023-11-29 12:39:35 +0200

    refactor form inputs

--- src/main/java/com/example/libraryproject/controllers/Controller.java
+++ src/main/java/com/example/libraryproject/controllers/Controller.java
@@ -2,6 +2,7 @@ package com.example.libraryproject.controllers;
 
 import com.example.libraryproject.entity.Librarian;
 import com.example.libraryproject.entity.Reader;
+import com.example.libraryproject.entity.Role;
 import com.example.libraryproject.services.ReaderService;
 import com.example.libraryproject.services.UserService;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -12,6 +13,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
@@ -46,11 +49,6 @@ public class Controller {
         };
     }
 
-    @GetMapping("/")
-    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('READER')")
-    public String goHome() {
-        return "Admin/newLibrarian/new_librarian.html";
-    }
 
 //    @PostMapping("/reader/save")
 //    public ResponseEntity<Object> saveUser(@RequestBody UserDto userDto) {
@@ -61,15 +59,11 @@ public class Controller {
 //        return userRepo.existsById(reader.getId()) ? ResponseEntity.ok("User was saved") : ResponseEntity.status(404).body("Can t save user");
 //    }
 
-    @GetMapping("/librarian")
-    @PreAuthorize("hasAuthority('LIBRARIAN')")
-    public String librarianPage() {
-        return "Hello librarian";
-    }
 
     @GetMapping("/admin")
     @PreAuthorize("hasAuthority('ADMIN')")
     public String adminPage(Model model) {
+        //TODO:дістати з бд
         ArrayList<Librarian> lib = new ArrayList<>();
         Librarian librarian = new Librarian();
         librarian.setEmail("qwe");
@@ -85,15 +79,40 @@ public class Controller {
         return "admin/main_page_admin";
     }
 
+    @GetMapping("/librarian/add")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public String getAddLibrarianPAge(Model model) {
+        return "admin/new_librarian";
+    }
+
+    @PostMapping("/librarian/add")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public RedirectView addLibrarian(@RequestParam("email") String email, @RequestParam String password, RedirectAttributes attributes) {
+        //TODO:перевірка в бд
+        Librarian librarian = new Librarian();
+        librarian.setRole(Role.LIBRARIAN);
+        librarian.setPassword(password);
+        librarian.setEmail(email);
+        attributes.addFlashAttribute("success", "Can t add librarian");
+        return new RedirectView("/admin");
+    }
+
     @GetMapping("/librarian/edit/{id}")
     @PreAuthorize("hasAuthority('ADMIN')")
-    public String editLibrarian(@PathVariable(value = "id") Long id, Model model) {
+    public String getEditLibrarianPage(@PathVariable(value = "id") Long id, Model model) {
         Librarian attributeValue = new Librarian();
         attributeValue.setEmail("someEmail");
         attributeValue.setPassword("pass");
+        attributeValue.setId(8L);
         model.addAttribute("librarian", attributeValue);
-        System.out.println("NICE");
         return "admin/edit_librarian";
+    }
+
+    @PostMapping("/librarian/edit/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public RedirectView editLibrarian(@RequestParam("email") String email, @RequestParam("password") String password, @PathVariable(value = "id") Long id, Model model) {
+        System.out.println(id);
+        return new RedirectView("/admin");
     }
 
     @GetMapping("/reader")
